f1b578f652afcfe48c3c9e1813aea6b27a1a771e
andrew.andrews <andrewardyour@gmail.com>
2025-11-25 19:30:44 +0500

    feat:dowload full work

--- users/urls.py
+++ users/urls.py
@@ -1,8 +1,14 @@
-from django.urls import path
-from .views import UserProfileUpdateView
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+
+from .views import UserProfileUpdateView, PaymentViewSet
 
 app_name = "users"
 
+router = DefaultRouter()
+router.register(r"payments", PaymentViewSet)
+
 urlpatterns = [
-    path("profile/int:pk/", UserProfileUpdateView.as_view(), name="profile-edit")
+    path("profile/<int:pk>/", UserProfileUpdateView.as_view(), name="profile-edit"),
+    path("", include(router.urls)),
 ]
